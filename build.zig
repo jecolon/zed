d0d90b044d5d9c5d91fb157d033c68d2e20d71a5
Jose Colon Rodriguez <jec.rod@gmail.com>
2022-04-12 22:09:20 -0400

    Updated build.zig for tests

--- build.zig
+++ build.zig
@@ -1,6 +1,44 @@
 const std = @import("std");
 const deps = @import("deps.zig");
 
+const pcre2_cfiles = &[_][]const u8{
+    "libs/pcre2-10.39/src/pcre2_auto_possess.c",
+    "libs/pcre2-10.39/src/pcre2_chartables.c",
+    "libs/pcre2-10.39/src/pcre2_compile.c",
+    "libs/pcre2-10.39/src/pcre2_config.c",
+    "libs/pcre2-10.39/src/pcre2_context.c",
+    "libs/pcre2-10.39/src/pcre2_convert.c",
+    "libs/pcre2-10.39/src/pcre2_dfa_match.c",
+    "libs/pcre2-10.39/src/pcre2_error.c",
+    "libs/pcre2-10.39/src/pcre2_extuni.c",
+    "libs/pcre2-10.39/src/pcre2_find_bracket.c",
+    "libs/pcre2-10.39/src/pcre2_jit_compile.c",
+    "libs/pcre2-10.39/src/pcre2_maketables.c",
+    "libs/pcre2-10.39/src/pcre2_match.c",
+    "libs/pcre2-10.39/src/pcre2_match_data.c",
+    "libs/pcre2-10.39/src/pcre2_newline.c",
+    "libs/pcre2-10.39/src/pcre2_ord2utf.c",
+    "libs/pcre2-10.39/src/pcre2_pattern_info.c",
+    "libs/pcre2-10.39/src/pcre2_script_run.c",
+    "libs/pcre2-10.39/src/pcre2_serialize.c",
+    "libs/pcre2-10.39/src/pcre2_string_utils.c",
+    "libs/pcre2-10.39/src/pcre2_study.c",
+    "libs/pcre2-10.39/src/pcre2_substitute.c",
+    "libs/pcre2-10.39/src/pcre2_substring.c",
+    "libs/pcre2-10.39/src/pcre2_tables.c",
+    "libs/pcre2-10.39/src/pcre2_ucd.c",
+    "libs/pcre2-10.39/src/pcre2_valid_utf.c",
+    "libs/pcre2-10.39/src/pcre2_xclass.c",
+};
+const pcre2_cflags = &[_][]const u8{
+    "-D",
+    "PCRE2_CODE_UNIT_WIDTH=8",
+    "-D",
+    "HAVE_CONFIG_H",
+    "-I",
+    "libs/pcre2-10.39/src",
+};
+
 pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
@@ -17,51 +55,9 @@ pub fn build(b: *std.build.Builder) void {
     exe.setBuildMode(mode);
     exe.setTarget(target);
     deps.addAllTo(exe);
-    //exe.strip = true;
-    exe.single_threaded = true;
-    //exe.addLibPath("/opt/homebrew/Cellar/pcre2/10.39/lib");
-    //exe.addIncludePath("/opt/homebrew/Cellar/pcre2/10.39/include");
-    exe.addIncludePath("libs/pcre2-10.39/src");
-    //exe.linkSystemLibrary("pcre2-8");
-
-    exe.addCSourceFiles(&.{
-        "libs/pcre2-10.39/src/pcre2_auto_possess.c",
-        "libs/pcre2-10.39/src/pcre2_chartables.c",
-        "libs/pcre2-10.39/src/pcre2_compile.c",
-        "libs/pcre2-10.39/src/pcre2_config.c",
-        "libs/pcre2-10.39/src/pcre2_context.c",
-        "libs/pcre2-10.39/src/pcre2_convert.c",
-        "libs/pcre2-10.39/src/pcre2_dfa_match.c",
-        "libs/pcre2-10.39/src/pcre2_error.c",
-        "libs/pcre2-10.39/src/pcre2_extuni.c",
-        "libs/pcre2-10.39/src/pcre2_find_bracket.c",
-        "libs/pcre2-10.39/src/pcre2_jit_compile.c",
-        "libs/pcre2-10.39/src/pcre2_maketables.c",
-        "libs/pcre2-10.39/src/pcre2_match.c",
-        "libs/pcre2-10.39/src/pcre2_match_data.c",
-        "libs/pcre2-10.39/src/pcre2_newline.c",
-        "libs/pcre2-10.39/src/pcre2_ord2utf.c",
-        "libs/pcre2-10.39/src/pcre2_pattern_info.c",
-        "libs/pcre2-10.39/src/pcre2_script_run.c",
-        "libs/pcre2-10.39/src/pcre2_serialize.c",
-        "libs/pcre2-10.39/src/pcre2_string_utils.c",
-        "libs/pcre2-10.39/src/pcre2_study.c",
-        "libs/pcre2-10.39/src/pcre2_substitute.c",
-        "libs/pcre2-10.39/src/pcre2_substring.c",
-        "libs/pcre2-10.39/src/pcre2_tables.c",
-        "libs/pcre2-10.39/src/pcre2_ucd.c",
-        "libs/pcre2-10.39/src/pcre2_valid_utf.c",
-        "libs/pcre2-10.39/src/pcre2_xclass.c",
-    }, &.{
-        "-D",
-        "PCRE2_CODE_UNIT_WIDTH=8",
-        "-D",
-        "HAVE_CONFIG_H",
-        "-I",
-        "libs/pcre2-10.39/src",
-    });
-
     exe.linkLibC();
+    exe.addIncludePath("libs/pcre2-10.39/src");
+    exe.addCSourceFiles(pcre2_cfiles, pcre2_cflags);
     exe.install();
 
     const run_cmd = exe.run();
@@ -78,9 +74,8 @@ pub fn build(b: *std.build.Builder) void {
     exe_tests.setBuildMode(mode);
     deps.addAllTo(exe_tests);
     exe_tests.linkLibC();
-    exe_tests.addLibPath("/opt/homebrew/Cellar/pcre2/10.39/lib");
-    exe_tests.addIncludePath("/opt/homebrew/Cellar/pcre2/10.39/include");
-    exe_tests.linkSystemLibrary("pcre2-8");
+    exe_tests.addIncludePath("libs/pcre2-10.39/src");
+    exe_tests.addCSourceFiles(pcre2_cfiles, pcre2_cflags);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
