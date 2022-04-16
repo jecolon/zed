20b76bd4277123dfabb3312b1d199ff3e265802a
Jose Colon Rodriguez <jec.rod@gmail.com>
2022-04-16 18:10:37 -0400

    Added missing files

---
+++ src/Context.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+const Location = @import("Location.zig");
+
+filename: []const u8,
+src: []const u8,
+
+const Context = @This();
+
+pub const seed = 2112;
+
+pub fn err(self: Context, comptime fmt: []const u8, args: anytype, e: anyerror, offset: u16) anyerror {
+    const location = Location.getLocation(self.filename, self.src, offset);
+    std.log.err(fmt, args);
+    std.debug.print("{}", .{location});
+    return e;
+}
